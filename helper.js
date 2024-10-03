6de1532f4af955b08d5c060181b27ba8750b1bf8
E-v7 <evanapogue@gmail.com>
2024-10-02 22:46:20 -0400

    Moved appsettings inside helper file

--- helper.js
+++ helper.js
@@ -1,9 +1,14 @@
 import { fileURLToPath } from 'url'
-import { dirname } from 'path'
+import { dirname, path } from 'path'
 
-// Get the absolute directory path to this app
+// Get the absolute directory path to this file
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
+
+/**
+ * @description A JSON object with the contents of the appsettings file
+ */
+export const appsettings = JSON.parse(fs.readFileSync(path.join(__dirname, 'appsettings.json')))
 
 /**
  * @description Deletes all files from the cache folder
