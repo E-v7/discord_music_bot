0464ee551801f180301189a4f15ec5a2da5fca06
E-v7 <33555329+E-v7@users.noreply.github.com>
2024-10-04 09:59:18 -0400

    Added logging system to use a log file to store useful information

--- helper.js
+++ helper.js
@@ -39,3 +39,45 @@ export function clearCache() {
         }
     })
 }
+
+export class Logger {
+    /**
+     * @type {string}
+     */
+    #log_file_path
+    constructor() {
+        this.#log_file_path = path.join(__dirname, 'howie.log')
+    }
+
+    /**
+     * @description Writes information into the log file
+     * 
+     * @param {string} info The info you want to insert into the log file
+     * @param {object[]} items This is optional 
+     */
+    Log(info, items = null) {
+        var date_time = Date.now().toLocaleString().replace(',', '')
+        var items_string = ''
+        if (items) {
+            items.forEach((item) => {
+                items_string += `${item} `
+            })
+        }
+
+        var final_log_message = `${date_time} ${info} ${items_string}`
+        fs.appendFile(this.#log_file_path, final_log_message, (err) => {
+            console.error('Could not write to the log file', err)
+        })
+    }
+
+    /**
+     * @description Logs an error to the log file with stack trace
+     * 
+     * @param {string} file The current file
+     * @param {Error} error The error caught
+     */
+    LogError(file, error) {
+        Error.captureStackTrace(error, this.LogError)
+        this.Log(file, error.stack)
+    }
+}
